a6074a2c52d4b5092924b6dcba7718b36f86c5bf
Samuel <samuelcat1478@gmail.com>
2023-08-15 02:31:27 +0800

    Fix merge error

--- README.md
+++ README.md
@@ -4,18 +4,28 @@
 
 ## 安裝依賴
 
+- Edge 瀏覽器
+
+- ffmpeg 命令
+
+- msspeech
+
 ```shell
-pip install msspeech pydub
-sudo apt install ffmpeg
+pip install msspeech
 ```
 
 ## 使用方式
 
+- 輸入 txt 輸出生成音頻
+
 ```shell
-# 輸入 txt 輸出生成音頻
-python text_to_sound.py <input> <output>
-# 顯示可用聲音
-python text_to_sound.py -p
+python text_to_sound.py <input.txt> <output.mp3>
+```
+
+- 顯示可用聲音
+
+```shell
+python text_to_sound.py -p <zh-CN|zh-TW|en-US...>
 ```
 
 ## 修改配置
@@ -30,7 +40,7 @@ VOICE_PITCH = 0
 # 設定聲音音量
 VOICE_VOLUME = 1.0
 # 緩存資料夾名稱
-TMP_DIR_NAME = "tmp"
+TMP_DIR_NAME = "text-to-sound_voice-tmp"
 # 無法直接轉換時，分割檔案的字數
 SPLIT_FILE_TEXT_SIZE = 19000
 ```
--- text_to_sound.py
+++ text_to_sound.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import asyncio
+import subprocess
 from msspeech import MSSpeech
-from pydub import AudioSegment
 
 # 設定聲音樣式
 VOICE_NAME = "zh-CN-YunxiNeural"
@@ -13,135 +13,147 @@ VOICE_PITCH = 0
 # 設定聲音音量
 VOICE_VOLUME = 1.0
 # 緩存資料夾名稱
-TMP_DIR_NAME = "tmp"
+TMP_DIR_NAME = "text-to-sound_voice-tmp"
 # 無法直接轉換時，分割檔案的字數
 SPLIT_FILE_TEXT_SIZE = 19000
 
-if len(sys.argv) < 2:
-	print("參數錯誤")
-	sys.exit(1)
-    
-if sys.argv[1] == '-h' or sys.argv[1] == '--help':
-	print("TextToSound.py <input> <output>  輸出文字聲音")
-	print("TextToSound.py -p                顯示可用聲音")
-	sys.exit(1)
-
-if sys.argv[1] == '-p':
-	IS_PRINT_VOICE = 1
-else:
-	IS_PRINT_VOICE = 0
-	INPUT_FILE_NAME = sys.argv[1]
-	OUTPUT_FILE_NAME = sys.argv[2]
-
 
 class MicrosoftVoice():
-	def __init__(self) -> None:
-		self.mss = MSSpeech()
+    def __init__(self) -> None:
+        self.mss = MSSpeech()
 
-	async def Initialize(self, voice = "zh-CN-YunxiNeural", rate = 1.0, pitch = 0, volume = 1.0):
-		await self.mss.set_voice(voice)
-		await self.mss.set_rate(rate)
-		await self.mss.set_pitch(pitch)
-		await self.mss.set_volume(volume)
+    async def initialize(self, voice="zh-CN-YunxiNeural", rate=1.0, pitch=0, volume=1.0):
+        await self.mss.set_voice(voice)
+        await self.mss.set_rate(rate)
+        await self.mss.set_pitch(pitch)
+        await self.mss.set_volume(volume)
 
-	async def TextToVoice(self, text:str, voice_file_name:str):
-		chunk_size = SPLIT_FILE_TEXT_SIZE
-		text_list = SplitString(text, chunk_size)
-		text_len = len(text_list)
-		text_len_count = len(str(text_len))
+    async def text_to_voice(self, text: str, voice_file_name: str):
+        chunk_size = SPLIT_FILE_TEXT_SIZE
+        text_list = self.__split_string(text, chunk_size)
+        text_len = len(text_list)
+        text_len_count = len(str(text_len))
 
-		# 創建緩存資料夾
-		tmp_dir_path = os.path.join(os.curdir, TMP_DIR_NAME)
-		if not os.path.exists(tmp_dir_path):
-			os.mkdir(tmp_dir_path)
-		
-		for i in range(text_len):
-			tmp_file_name = ("{:0" + str(text_len_count) + "d}.mp3").format(i)
-			tmp_file_path = os.path.join(tmp_dir_path, tmp_file_name)
-			# 生成音頻
-			await self.mss.synthesize(text_list[i].strip(), tmp_file_path)
-		
-		MergeAudio(tmp_dir_path, voice_file_name)
+        # 創建緩存資料夾
+        tmp_dir_path = os.path.join(os.curdir, TMP_DIR_NAME)
+        if not os.path.exists(tmp_dir_path):
+            os.mkdir(tmp_dir_path)
 
-		# 刪除緩存資料夾
-		RemoveFolder(tmp_dir_path)
+        for i in range(text_len):
+            tmp_file_name = ("{:0" + str(text_len_count) + "d}.mp3").format(i)
+            tmp_file_path = os.path.join(tmp_dir_path, tmp_file_name)
+            # 生成音頻
+            await self.mss.synthesize(text_list[i].strip(), tmp_file_path)
 
-# 分割字串 text 按照 chunk_size 長度分割為 子字串
-def SplitString(text, chunk_size):
-	chunks = []
-	current_chunk = ""
+        self.__merge_audio(tmp_dir_path, voice_file_name)
 
-	for char in text:
-		current_chunk += char
-		if len(current_chunk) >= chunk_size and char == '\n':
-			chunks.append(current_chunk)
-			current_chunk = ""
+        # 刪除緩存資料夾
+        self.__remove_folder(tmp_dir_path)
 
-	if current_chunk:
-		chunks.append(current_chunk)
+    def __split_string(self, text, chunk_size):
+        '''分割字串 text 按照 chunk_size 長度分割為 子字串'''
+        chunks = []
+        current_chunk = ""
 
-	return chunks
+        for char in text:
+            current_chunk += char
+            if len(current_chunk) >= chunk_size and char == '\n':
+                chunks.append(current_chunk)
+                current_chunk = ""
 
-# 合併音頻
-def MergeAudio(source_path, output_path, file_extension = ".mp3"):
-	original_dir = os.getcwd()
-	os.chdir(source_path)
+        if current_chunk:
+            chunks.append(current_chunk)
 
-	# 讀取文件並排序
-	audio_files = [file for file in os.listdir() if file.endswith(file_extension)]
-	audio_files.sort()
+        return chunks
 
-	# 建立一個空的音頻段
-	combined_audio = AudioSegment.empty()
-	# 依序合併音頻文件
-	for file in audio_files:
-		audio = AudioSegment.from_file(file)
-		combined_audio += audio
-
-	os.chdir(original_dir)
-	# 輸出合併後的音頻檔案
-	combined_audio.export(output_path, format="mp3", bitrate="48k")
-
-	
-
-# 刪除指定資料夾，包底下檔案
-def RemoveFolder(folder_path):
-    # 確保資料夾存在
-    if os.path.exists(folder_path):
-        # 取得資料夾中的所有檔案
-        file_list = os.listdir(folder_path)
+    def __merge_audio(self, source_path, output_path, file_extension=".mp3"):
+        '''合併音頻'''
         
-        # 刪除所有檔案
-        for file_name in file_list:
-            file_path = os.path.join(folder_path, file_name)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-        
-        # 刪除根資料夾
-        os.rmdir(folder_path)
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        output_path = os.path.abspath(output_path)
 
-# 顯示所有可用的聲音語言
-async def ShowVoiceList(locale = "zh-CN"):
-	mss = MSSpeech()
-	voices = await mss.get_voices_list()
-	print("可用配音：")
-	for voice in voices:
-		if voice["Locale"] == locale:
-			print(voice["Name"])
+        original_dir = os.getcwd()
+        os.chdir(source_path)
 
-async def main():
-	if IS_PRINT_VOICE:
-		await ShowVoiceList()
-	else:
-		with open(INPUT_FILE_NAME, "r", encoding="UTF-8") as f:
-			text = f.read()
+        # 讀取文件並排序
+        audio_files = [
+            file for file in os.listdir() if file.endswith(file_extension)
+        ]
+        audio_files.sort()
 
-		voice = MicrosoftVoice()
-		await voice.Initialize(VOICE_NAME, VOICE_RATE, VOICE_PITCH, VOICE_VOLUME)
-		await voice.TextToVoice(text, OUTPUT_FILE_NAME)
+        input_files = "concat:"
+        for file in audio_files:
+            input_files = input_files + file + '|'
+        input_files = input_files[:-1]
 
+        command = f'ffmpeg -i "{input_files}" -c copy "{output_path}"'
+        subprocess.run(command, shell=True)
+
+        os.chdir(original_dir)
+
+    def __remove_folder(self, folder_path):
+        '''刪除指定資料夾，包含底下檔案'''
+        # 確保資料夾存在
+        if os.path.exists(folder_path):
+            # 取得資料夾中的所有檔案
+            file_list = os.listdir(folder_path)
+
+            # 刪除所有檔案
+            for file_name in file_list:
+                file_path = os.path.join(folder_path, file_name)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+
+            # 刪除根資料夾
+            os.rmdir(folder_path)
+
+
+async def ShowVoiceList(locale:str = ""):
+    '''顯示所有可用的聲音語言'''
+    mss = MSSpeech()
+    print("可用配音：")
+
+    voices = await mss.get_voices_list()
+    for voice in voices:
+        if voice["Locale"] == locale:
+            print(voice["Name"])
+        elif locale == "":
+            print(voice["Name"])
+
+
+async def TextToAudio():
+    '''生成音頻檔'''
+    with open(INPUT_FILE_NAME, "r", encoding="UTF-8") as f:
+        text = f.read()
+
+    voice = MicrosoftVoice()
+    await voice.initialize(VOICE_NAME, VOICE_RATE, VOICE_PITCH, VOICE_VOLUME)
+    await voice.text_to_voice(text, OUTPUT_FILE_NAME)
 
 
 if __name__ == "__main__":
-	loop = asyncio.get_event_loop()
-	loop.run_until_complete(main())
+    if len(sys.argv) < 2:
+        print("參數錯誤")
+
+    elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        print("text_to_sound.py <input.txt> <output.mp3>    輸出文字聲音")
+        print("text_to_sound.py -p <zh-CN|zh-Tw|en-US...>   顯示可用聲音")
+
+    elif sys.argv[1] == '-p':
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(ShowVoiceList(sys.argv[2]))
+        except IndexError:
+            loop.run_until_complete(ShowVoiceList())
+
+    elif os.path.exists(sys.argv[1]) and \
+            os.path.basename(sys.argv[1]):
+        INPUT_FILE_NAME = sys.argv[1]
+        OUTPUT_FILE_NAME = sys.argv[2]
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(TextToAudio())
+
+    else:
+        print("參數錯誤")
+
+    sys.exit(1)
